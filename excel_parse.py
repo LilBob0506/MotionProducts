f367d7747a79534a6b94afaf73134ce2ba1e5442
Hunter Reinhart <hreinhartcodes@gmail.com>
2025-02-06 14:35:36 -0600

    Add parse and spreadsheet

---
+++ excel_parse.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_excel("List.xlsx", nrows=1)
+#nrows controls the number of rows to read
+
+data = df[["[<ID>]", "ITEM_NO", "Part Number", "MFR_NAME"]].values.tolist()
